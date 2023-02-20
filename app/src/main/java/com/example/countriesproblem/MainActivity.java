234fa9f917641df343f92bf3440302d5c5162ae3
StringOfChars <zelinsky91@gmail.com>
2023-02-20 20:48:22 +0200

    finalized

--- app/src/main/java/com/example/countriesproblem/MainActivity.java
+++ app/src/main/java/com/example/countriesproblem/MainActivity.java
@@ -23,21 +23,27 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 0; i < countries.length(); i++) {
             char x = countries.charAt(i);
+
+            if ((i <= countries.length() - 3 && (Objects.equals(countries.charAt(i + 1), 'a') && Objects.equals(countries.charAt(i + 2), 'n') &&
+                    Objects.equals(countries.charAt(i + 3), 'd') && Objects.equals(countries.charAt(i + 4), ' ')))
+                    || (i > 3 && (Objects.equals(countries.charAt(i - 1), 'd') && Objects.equals(countries.charAt(i - 2), 'n') &&
+                    Objects.equals(countries.charAt(i - 3), 'a') && Objects.equals(countries.charAt(i - 4), ' ')))
+                    || potentialCountry.matches("American") || potentialCountry.matches("European")) {
+                potentialCountry += x;
+                continue;
+            }
+
+            if (x == ' ') {
+                allCountries[idx] = potentialCountry;
+                potentialCountry = "";
+                idx++;
+                continue;
+            }
+
             potentialCountry += x;
+
             if (x == countries.charAt(countries.length() - 1)) {
                 allCountries[idx] = potentialCountry;
-            }
-            if (x == ' ') {
-                allCountries[idx] = potentialCountry.substring(0, potentialCountry.length() - 1);
-                if (Objects.equals(countries.charAt(i + 1), 'a') && (Objects.equals(countries.charAt(i + 2), 'n') &&
-                        (Objects.equals(countries.charAt(i + 3), 'd') && (Objects.equals(countries.charAt(i + 4), ' '))))
-                        || (Objects.equals(countries.charAt(i - 1), 'd') && (Objects.equals(countries.charAt(i - 2), 'n') &&
-                        (Objects.equals(countries.charAt(i - 3), 'a') && (Objects.equals(countries.charAt(i - 4), ' ')))))) {
-                } else if ((allCountries[idx].matches("American")) || ((allCountries[idx].matches("European")))) {
-                } else {
-                    potentialCountry = "";
-                    idx++;
-                }
             }
         }
         String allCountryNames = "";
@@ -47,33 +53,6 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(allCountryNames.substring(0, allCountryNames.length() - 2) + ".");
     }
 }
-
-
-//           allCountries[token] = strObject.nextElement().toString(); //@TODO with custom String country parser
-//            //@TODO prettier for android Studio
-//        for (int token = 0; token < allCountries.length; token++) {
-//
-//            if (allCountries[token].matches("and")) {
-//                allCountries[token -1] =   allCountries[token -1] + " " + allCountries[token] +  " " + allCountries[token+1];
-//                for (int split = token; split < allCountries.length-2; split++) {
-//                    allCountries [split] = allCountries[split+2];
-//                   } break;
-//               }
-//            if ((allCountries[token].matches("American"))||(allCountries[token].matches("European"))
-//                    ||(allCountries[token].matches("United")))
-//            {
-//                allCountries[token] = allCountries[token] + " " + allCountries[token+1];
-//                for (int split = token + 1; split < allCountries.length-2; split++) {
-//                    allCountries [split] = allCountries[split+1];
-//                } break;
-//            }}
-//        String allCountryNames = "";
-//
-//        for (String countryName : allCountries) {
-//
-//            allCountryNames += "\"" + countryName + "\", ";
-//        }
-//        System.out.println(allCountryNames.substring(0, allCountryNames.length()-2) + ".");
 
 
 
